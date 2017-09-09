5a522b6037e580db80cc1b7b2014795e3fc1ac86
Ratna Halder <ratnacse06@gmail.com>
2017-09-09 13:53:52 -0400

    now, error message also added on output file

--- source/execute.py
+++ source/execute.py
@@ -12,7 +12,7 @@ hex2dec_arguments = cfg.execute['hex2dec_arguments']
 f_dec2hex = open(cfg.execute['dec2hex_output_file'],'w')
 for argument in dec2hex_arguments: #loop for input values
     #temp = subprocess.call([program, argument])
-    res = subprocess.Popen([dec2hex_program, argument], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    res = subprocess.Popen([dec2hex_program, argument], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
     result = res.communicate(argument)[0]
     f_dec2hex.write(result) # write output into a text file        
 f_dec2hex.close()# file writing completed  
@@ -20,9 +20,8 @@ f_dec2hex.close()# file writing completed
 #execute hex2dec .exe file with argument/input and write the output into a file 
 f_hex2dec = open(cfg.execute['hex2dec_output_file'],'wb')
 for argument in hex2dec_arguments:
-    res = subprocess.Popen([hex2dec_program, argument], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    result = (res.communicate(argument)[0]).strip('\x00') #remove extra '\x00' from output string 
-    #result = result.rstrip('\x00') 
+    res = subprocess.Popen([hex2dec_program, argument], stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, close_fds=True)
+    result = (res.communicate(argument)[0]).strip('\x00') #remove extra '\x00' from output string   
     #f_hex2dec.write(bytearray(int(result, 16)))
     f_hex2dec.write(result)         
 f_hex2dec.close() # file writing completed
